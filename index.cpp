89184a6e04c41ce6c65858270b3541e67cce7e84
amirtedy <amirtedy83@gmail.com>
2026-05-20 13:36:40 +0330

    so, what happend to conflicts, again now

--- index.cpp
+++ index.cpp
@@ -1,12 +1,9 @@
 #include <iostream>
 using namespace std;
 int main() {
-<<<<<<< HEAD
     cout << "hello world";
-=======
     int x = 3;
     float y = 2.7;
     cout << y / x << "\n";
->>>>>>> df3833a17f0fa0ff973c84ea7d036a56ca57f450
     return 0;
 }
